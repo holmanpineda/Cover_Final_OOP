a24de801054963d8f2b2e8f0a63bdd43a674f2b6
holman <pinedaholmandavid12@gmail.com>
2025-12-10 09:51:22 -0500

    test hasta buscar elemento

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -136,23 +136,47 @@ public class Workshop {
 
 
     // Método que encuentra el elemento mayor en un arreglo
-    public int encontrarElementoMayor(int[] arreglo) {
-        // TODO: Implementar el método para encontrar el elemento mayor en un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 5.
-        return 0;
-    }
+            public int encontrarElementoMayor(int[] arreglo) {
+                // TODO: Implementar el método para encontrar el elemento mayor en un arreglo.
+                // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 5.
+                int mayor = arreglo[0];
+
+                for (int i = 1; i < arreglo.length; i++) {
+                    if (arreglo[i] > mayor) {
+                        mayor = arreglo[i];
+                    }
+                }
+
+                return mayor;
+            }
+
 
     // Método que encuentra el elemento menor en un arreglo
     public int encontrarElementoMenor(int[] arreglo) {
         // TODO: Implementar el método para encontrar el elemento menor en un arreglo.
         // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 1.
-        return 0;
+        int menor = arreglo[0];
+
+        for (int i = 1; i < arreglo.length; i++) {
+            if (arreglo[i] < menor) {
+                menor = arreglo[i];
+            }
+        }
+
+        return menor;
     }
+
 
     // Método que busca un elemento en un arreglo
     public boolean buscarElemento(int[] arreglo, int elemento) {
         // TODO: Implementar el método para buscar un elemento en un arreglo.
         // Ejemplo: Si arreglo = [1, 2, 3, 4, 5] y elemento = 3, el resultado debería ser true.
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] == elemento) {
+                return true;
+            }
+        }
+
         return false;
     }
 
